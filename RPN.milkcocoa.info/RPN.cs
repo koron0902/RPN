05732c782988255b922c2d0362e050c1675bf2b4
Keita <koron0902@gmail.com>
2020-11-25 00:53:04 +0900

    単体検査を導入

--- RPN.milkcocoa.info/RPN.cs
+++ RPN.milkcocoa.info/RPN.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-namespace FlickCalc {
+namespace RPN {
   public class Token {
     public enum Kind {
       Number,
@@ -19,11 +19,13 @@ namespace FlickCalc {
     Regex op_;
     Regex cos_;
     Regex sin_;
+    Regex num_;
     /// summary   : コンストラクタ
     public RPN() {
-      op_ = new Regex(@"([+\-×÷=()])");
-      sin_ = new Regex(@"(~?[0-9]+\.?[0-9]+?)sin");
-      cos_ = new Regex(@"(~?[0-9]+\.?[0-9]+?)cos");
+      op_ = new Regex(@"([+\-×*÷/=()])");
+      sin_ = new Regex(@"sin$");
+      cos_ = new Regex(@"cos$");
+      num_ = new Regex(@"(-?[0-9]?\.[0-9]?|[0-9]*)");
     }
 
 
@@ -40,14 +42,14 @@ namespace FlickCalc {
 
           if(t.Equals("(") | t.Equals(")")) {
             priority = 4;
-          } else if(t.Equals("÷")) {
+          } else if(t.Equals("÷") || t.Equals("/")) {
             priority = 3;
-          } else if(t.Equals("×")) {
+          } else if(t.Equals("×") || t.Equals("*")) {
             priority = 2;
           } else {
             priority = 1;
           }
-          tokenized.Add(new Token() { kind_ = Token.Kind.Operator, value_ = t, priority_ = priority });
+          tokenized.Add(new Token() { kind_ = Token.Kind.Operator, value_ = t.Replace("×", "*").Replace("÷", "/"), priority_ = priority });
         } else if(Regex.IsMatch(t, cos_.ToString()) |
                   Regex.IsMatch(t, sin_.ToString())) {
           tokenized.Add(new Token() { kind_ = Token.Kind.Function, value_ = t.Replace("~", "-"), priority_ = 5 });
@@ -93,7 +95,7 @@ namespace FlickCalc {
       return converted;
     }
 
-    private String Calc(IEnumerable<Token> _token) {
+    private double Calc(IEnumerable<Token> _token) {
       var stack = new Stack<double>();
 
 
@@ -104,17 +106,25 @@ namespace FlickCalc {
           if(Regex.IsMatch(t.value_, cos_.ToString())) {
             var arg = stack.Pop();
             double amp;
-            if(!double.TryParse(Regex.Match(t.value_, cos_.ToString()).Value, out amp)) {
-              // 係数がない場合は1倍として扱う
+            var ampStr = Regex.Match(t.value_, num_.ToString()).Value;
+            if(ampStr.Equals("")) {
               amp = 1;
+            } else if(ampStr.Equals(".")) {
+              throw new Exception("この計算はできません．");
+            } else {
+              amp = double.Parse(ampStr);
             }
             stack.Push(amp * Math.Cos(arg));
           } else if(Regex.IsMatch(t.value_, sin_.ToString())) {
             var arg = stack.Pop();
             double amp;
-            if(!double.TryParse(Regex.Match(t.value_, sin_.ToString()).Value, out amp)) {
-              // 係数がない場合は1倍として扱う
+            var ampStr = Regex.Match(t.value_, num_.ToString()).Value;
+            if(ampStr.Equals("")) {
               amp = 1;
+            } else if(ampStr.Equals(".")) {
+              throw new Exception("この計算はできません．");
+            } else {
+              amp = double.Parse(ampStr);
             }
             stack.Push(amp * Math.Sin(arg));
           } else {
@@ -126,6 +136,9 @@ namespace FlickCalc {
           if(stack.Count() >= 2) {
             rhs = stack.Pop();
             lhs = stack.Pop();
+          } else if(stack.Count == 1 && t.value_.Equals("-")) {
+            lhs = 0;
+            rhs = stack.Pop();
           } else {
             throw new Exception("この計算はできません．");
           }
@@ -135,9 +148,9 @@ namespace FlickCalc {
             stack.Push(lhs + rhs);
           } else if(t.value_.Equals("-")) {
             stack.Push(lhs - rhs);
-          } else if(t.value_.Equals("×")) {
+          } else if(t.value_.Equals("*")) {
             stack.Push(lhs * rhs);
-          } else if(t.value_.Equals("÷")) {
+          } else if(t.value_.Equals("/")) {
             if(rhs == 0) {
               throw new Exception("この計算はできません．");
             }
@@ -148,12 +161,15 @@ namespace FlickCalc {
         }
       }
 
-      return stack.Last().ToString("#############0.##############");
+      if(stack.Count != 1) {
+        throw new Exception("この計算はできません．");
+      }
+      return stack.Last();
     }
 
 
 
-    public String Proc(string _formula) {
+    public double Proc(string _formula) {
       return Calc(Convert(Tokenize(_formula)));
     }
   }
